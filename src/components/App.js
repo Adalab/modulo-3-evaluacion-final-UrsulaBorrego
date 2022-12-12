558a435d2c5277683a2b13e33ff7ed3d976e1af8
Ursula Borrego <ursulaborrego@hotmail.com>
2022-12-12 17:59:36 +0000

    Call to Api with Fetch

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,18 @@
-import '../styles/App.scss';
+// import "../styles/App.scss";
+import { useEffect, useState } from "react";
+import getDataFromApi from "../services/api";
 
 function App() {
-  return <div className="App">Rellename!</div>
-}
+  const [dataCharacter, setDataCharacter] = useState([]);
 
+  useEffect(() => {
+    getDataFromApi().then((cleanData) => {
+      console.log(cleanData);
+      setDataCharacter(cleanData);
+    });
+  }, []);
+
+  return <div className="App">Rellename!</div>;
+}
 
 export default App;
---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+const getDataFromApi = () => {
+  return fetch(
+    // ESTE ENLACE ES EL SEGURO, CAMBIAR POR EL DE LA API:  "https://rickandmortyapi.com/api/character"
+    "https://raw.githubusercontent.com/Adalab/rick-y-morty/master/data/rick-y-morty.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const cleanData = data.results.map((character) => {
+        return {
+          photo: character.image,
+          name: character.name,
+          specie: character.species,
+          id: character.id,
+        };
+      });
+      return cleanData;
+    });
+};
+
+export default getDataFromApi;
